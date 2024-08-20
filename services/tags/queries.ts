6c8daaf69e399ef3adacb9483691f4b811694b3a
Dylan Lindsay <dylan.lindsay234@gmail.com>
2024-08-20 13:59:20 +1000

    moved type definitions to its own file

--- services/tags/queries.ts
+++ services/tags/queries.ts
@@ -1,25 +1,14 @@
 import { createAxiosInstance } from "@/constants/primitives";
-
-type fetchTagsParams = {
-  page: number;
-  limit: number;
-};
-
-type fetchTagsResponse = {
-  data: {
-    name: string;
-  }[];
-  total: number;
-};
+import { TagsParam, TagsResponse } from "@/types/tags";
 
 namespace TagService {
   export const getTags = async ({
     page,
     limit,
-  }: fetchTagsParams): Promise<fetchTagsResponse> => {
+  }: TagsParam): Promise<TagsResponse> => {
     try {
       const axiosInstance = await createAxiosInstance();
-      const response = await axiosInstance.get<fetchTagsResponse>(
+      const response = await axiosInstance.get<TagsResponse>(
         `/tags?page=${page}&limit=${limit}`
       );
       return response.data;
