7a017bcf22328f2ec45292b5d65248ffc27ec8d6
Dylan Lindsay <dylan.lindsay234@gmail.com>
2024-08-20 12:55:40 +1000

    feat: :sparkles: created axios getTags function
    this function retrieves all the tags from the API

---
+++ services/tags/queries.ts
@@ -0,0 +1,30 @@
+import { createAxiosInstance } from "@/constants/primitives";
+
+type fetchTagsParams = {
+  page: number;
+  limit: number;
+};
+
+type fetchTagsResponse = {
+  data: {
+    name: string;
+  }[];
+  total: number;
+};
+
+namespace TagService {
+  export const getTags = async ({ page, limit }: fetchTagsParams) => {
+    try {
+      const axiosInstance = await createAxiosInstance();
+      const response = await axiosInstance.get<fetchTagsResponse>(
+        `/tags?page=${page}&limit=${limit}`
+      );
+      return response.data;
+    } catch (error) {
+      console.error("Error fetching tags:", error);
+      throw new Error("Failed to fetch tags");
+    }
+  };
+}
+
+export default TagService;
