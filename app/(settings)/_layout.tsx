cc23abf5604d96362a305b461a6c796b2e32bd04
Dylan Lindsay <dylan.lindsay234@gmail.com>
2024-08-26 14:38:24 +1000

    cleaned up headerShown for each stack into one

--- app/(settings)/_layout.tsx
+++ app/(settings)/_layout.tsx
@@ -1,17 +1,13 @@
 import { Stack } from "expo-router";
-import React from "react";
 
 const SettingsLayout = () => {
   return (
     <>
-      <Stack>
-        <Stack.Screen name="profile" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="personal-information"
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen name="location" options={{ headerShown: false }} />
-        <Stack.Screen name="update-password" options={{ headerShown: false }} />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="profile" />
+        <Stack.Screen name="personal-information" />
+        <Stack.Screen name="location" />
+        <Stack.Screen name="update-password" />
       </Stack>
     </>
   );
