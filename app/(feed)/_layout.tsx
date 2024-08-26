e397ce3822f638593f0480c5d0de96b55c1b9154
Dylan Lindsay <dylan.lindsay234@gmail.com>
2024-08-26 14:39:28 +1000

    cleaned up headerShown for each stack screen

--- app/(feed)/_layout.tsx
+++ app/(feed)/_layout.tsx
@@ -1,15 +1,14 @@
 import { Stack } from "expo-router";
-import React from "react";
 
 const FeedLayout = () => {
   return (
-    <Stack>
-      <Stack.Screen name="forum" options={{ headerShown: false }} />
-      <Stack.Screen name="post-expanded" options={{ headerShown: false }} />
-      <Stack.Screen name="create-post" options={{ headerShown: false }} />
-      <Stack.Screen name="category-post" options={{ headerShown: false }} />
-      <Stack.Screen name="review-post" options={{ headerShown: false }} />
-      <Stack.Screen name="edit-post" options={{ headerShown: false }} />
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="forum" />
+      <Stack.Screen name="post-expanded" />
+      <Stack.Screen name="create-post" />
+      <Stack.Screen name="category-post" />
+      <Stack.Screen name="review-post" />
+      <Stack.Screen name="edit-post" />
     </Stack>
   );
 };
