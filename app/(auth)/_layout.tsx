44c77214055b54a4ad8142c33d7a84c20bb8ab30
Dylan Lindsay <dylan.lindsay234@gmail.com>
2024-08-27 14:38:49 +1000

    refactor: :recycle: cleaned up screen options headers

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,24 +1,16 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 import React from "react";
 
 const AuthLayout = () => {
   return (
     <>
-      <Stack>
-        <Stack.Screen name="sign-in" options={{ headerShown: false }} />
-        <Stack.Screen name="sign-up" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="sign-up-location"
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="sign-up-password"
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="sign-up-profile-image"
-          options={{ headerShown: false }}
-        />
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="sign-in" />
+        <Stack.Screen name="sign-up" />
+        <Stack.Screen name="sign-up-location" />
+        <Stack.Screen name="sign-up-password" />
+        <Stack.Screen name="sign-up-profile-image" />
       </Stack>
     </>
   );
